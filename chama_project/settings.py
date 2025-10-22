7004cc8adacd17abb089beefea1a1bb398bad58e
LAFlame594 <lilflame694@gmail.com>
2025-10-22 21:43:12 +0300

    removed the local db for testing

--- chama_project/settings.py
+++ chama_project/settings.py
@@ -98,23 +98,14 @@ WSGI_APPLICATION = 'chama_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if DEBUG:
-    # Use SQLite for local development
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    # Use PostgreSQL for production
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.getenv("DATABASE_URL"),
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
+# Use PostgreSQL for production
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 
 
